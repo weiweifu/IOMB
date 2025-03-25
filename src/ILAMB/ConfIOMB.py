06e0072f70fa1cd7deffcdfa9262d81257325e17
weiweifu <weiweif@uci.edu>
2025-03-25 14:06:02 +0800

    add xarray and ipython to ilamb.yml; remove xarray from ConfIOMB

--- src/ILAMB/ConfIOMB.py
+++ src/ILAMB/ConfIOMB.py
@@ -19,7 +19,6 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import xarray as xr
 from   cf_units import Unit
 from   netCDF4 import Dataset
 
